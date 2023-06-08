29b790117b003240bcb214b107314145907bd621
Kayla Tom <kayla.tom@duke.edu>
2023-06-08 13:07:40 -0400

    added another card

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,31 +1,52 @@
 <script>
 
 </script>
-
-<div class = "card">
-        <div>
-        <img src="Elna Spaulding.jpg" alt="portrait of Elna Spaulding">
+<section class = "cards">
+    <div class = "card">    
+        <div class = "image">
+            <img src="Elna Spaulding.jpg" alt="portrait of Elna Spaulding">
         </div>
-
-        <div>
+        <div class = "info">
             <h1> Elna Spaulding </h1>
             <h3>1909-2007</h3>
             <p>First black female member of the Durham County Commission who served from 1974-1984. 
             She was the founding president of Women-In-Action for the Prevention of Violence and 
             Its Causes which was instrumental in promoting school desegregation in Durham.</p>
         </div>
+    </div>
+
+    <div class = "card">
+        <div class = image>
+            <img src="Benjamin Reese.jpg" alt="portrait of Benjamin Reese">
+        </div>
+        <div class = "info">
+            <h1> Benjamin Reese </h1>
+            <h3>(   -    )</h3>
+            <p>Benjamin Reese (___) was a psychologist who served for 14 years as Duke University’s 
+            Vice President for Institutional Equality. He worked to address issues of inequity on
+             campus, brought more diversity to the faculty, and addressed sexual harassment issues.</p>
+        </div>
+    </div>
+</section>
+
+
+        
         
     
-</div>
 
 <style>
     .card{
         background-color: aliceblue;
         padding: 10px;
         border-radius: 10px;
+        max-width: 40%;
+        box-shadow: 10px 10px blue;
         display: flex;
         align-items: center;
-
+    }
+    .cards{
+        display: flex;
+        justify-content: space-evenly;
     }
 
     img{
