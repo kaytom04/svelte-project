366c1abca1db44f222666646dd10bfcf4ebe1da9
Kayla Tom <kayla.tom@duke.edu>
2023-06-08 12:47:52 -0400

    added flexbox to css

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -3,12 +3,19 @@
 </script>
 
 <div class = "card">
-    <img src="Elna Spaulding.jpg" alt="portrait of Elna Spaulding">
-    <h1> Elna Spaulding </h1>
-        <h3>1909-2007</h3>
-        <p>First black female member of the Durham County Commission who served from 1974-1984. 
-        She was the founding president of Women-In-Action for the Prevention of Violence and 
-        Its Causes which was instrumental in promoting school desegregation in Durham.</p>
+        <div>
+        <img src="Elna Spaulding.jpg" alt="portrait of Elna Spaulding">
+        </div>
+
+        <div>
+            <h1> Elna Spaulding </h1>
+            <h3>1909-2007</h3>
+            <p>First black female member of the Durham County Commission who served from 1974-1984. 
+            She was the founding president of Women-In-Action for the Prevention of Violence and 
+            Its Causes which was instrumental in promoting school desegregation in Durham.</p>
+        </div>
+        
+    
 </div>
 
 <style>
@@ -16,10 +23,14 @@
         background-color: aliceblue;
         padding: 10px;
         border-radius: 10px;
+        display: flex;
+        align-items: center;
+
     }
 
     img{
-        max-width: 20%;
+        max-width: 75%;
+        height: auto;
     }
 </style>
 
